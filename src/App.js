393914e903a59d85fb56ef5864ea0e6b6b5b0fad
PameSegovia <114256835+PameSegovia@users.noreply.github.com>
2024-11-05 17:46:59 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import imageRickMorty from './img/rick-morty.png';
+import './App.css';
+
+function App() {
+  const reqApi = () => {
+    console.log("Clicking")
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        <img src={imageRickMorty} alt="Rick & Morty" className="img-home"/>
+        <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
